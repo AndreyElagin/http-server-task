f3bb58396ccc0bcdd4b2161926c0925228f4da9c
mrPTqp <mrPTqp@gmail.com>
2020-01-27 23:14:20 +0300

    Adjust SocketHandlerImplTest to write to file. Adjust SocketHandlerImplTest to read from a file

--- src/test/java/com/study/server/SocketHandlerImplTest.java
+++ src/test/java/com/study/server/SocketHandlerImplTest.java
@@ -4,11 +4,7 @@ import com.study.server.utils.RequestDispatcherMock;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
 import static com.study.server.utils.TestUtils.readFile;
@@ -20,7 +16,6 @@ class SocketHandlerImplTest {
     void test() throws IOException {
 //        тебе надо как то проверить:
 //        а) то что вызов был передан в реквест диспатчер
-//        б) то что респонс был записан в аутпут стрим
 
         Socket clientSocket = new Socket() {
             @Override
@@ -30,7 +25,8 @@ class SocketHandlerImplTest {
 
             @Override
             public OutputStream getOutputStream() throws IOException {
-                File file = new File("/src/test/resources/output");
+                File file = new File(System.getProperty("user.dir") + "\\src\\test\\resources\\output");
+                file.delete();
                 OutputStream os = new FileOutputStream(file) {
 
                     @Override
@@ -47,13 +43,15 @@ class SocketHandlerImplTest {
 
         sh.run();
 
-//        File file = new File("output");
-//        var in = new FileInputStream(file);
-//        var br = new BufferedReader(new InputStreamReader(in));
-//        var curLine = br.readLine();
-//        while (!curLine.equals("")) {
-//            System.out.println(curLine);
-//        }
-//        System.out.println(file);
+        File file = new File(System.getProperty("user.dir") + "\\src\\test\\resources\\output");
+        var in = new FileInputStream(file);
+        var br = new BufferedReader(new InputStreamReader(in));
+        StringBuilder sb = new StringBuilder();
+        var curLine = br.readLine();
+        while (curLine != null) {
+            sb.append(curLine + "\r\n");
+            curLine = br.readLine();
+        }
+        System.out.println(sb);
     }
 }
